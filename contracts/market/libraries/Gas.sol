2482196a39025092859d32401573369bad419370
rightjelkin <romgalkin23@gmail.com>
2021-10-06 12:27:45 +0700

    fix: some gas amount changes according on mainnet testing

--- contracts/market/libraries/Gas.sol
+++ contracts/market/libraries/Gas.sol
@@ -8,7 +8,7 @@ library Gas {
     // tip3
     uint128 constant DEPLOY_EMPTY_WALLET_VALUE      = 0.2 ton;
     uint128 constant DEPLOY_EMPTY_WALLET_GRAMS      = 0.1 ton;
-    uint128 constant GET_WALLET_ADDRESS_VALUE       = 1 ton;
+    uint128 constant GET_WALLET_ADDRESS_VALUE       = 1.2 ton;
     uint128 constant SET_RECEIVE_CALLBACK_VALUE     = 0.5 ton;
     uint128 constant GET_TOKEN_WALLET_DETAILS       = 0.5 ton;
     uint128 constant TRANSFER_TO_RECIPIENT_VALUE    = 0.2 ton;
